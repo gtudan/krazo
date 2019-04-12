38b70e382553f2d2dae349e6b3d59335b13e284d
Gregor Tudan <gregor@tudan.de>
2019-04-12 12:15:41 +0200

    configure travis build for liberty

--- .travis/docker-liberty.sh
+++ .travis/docker-liberty.sh
@@ -3,5 +3,5 @@
 set -eu
 
 docker run -p 9080:9080 -p 9443:9443 \
-  -v /${TRAVIS_BUILD_DIR}/.travis/wlp-server-template.xml:/config \
+  -v /${TRAVIS_BUILD_DIR}/.travis/wlp-server-template.xml:/config/server.xml \
   open-liberty:webProfile8
