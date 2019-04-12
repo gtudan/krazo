8808025f85d3cea36250ee4c071bfba7c1ef61d6
Gregor Tudan <gregor@tudan.de>
2019-04-12 12:54:22 +0200

    run liberty in the background

--- .travis/docker-liberty.sh
+++ .travis/docker-liberty.sh
@@ -2,6 +2,6 @@
 
 set -eu
 
-docker run -p 9080:9080 -p 9443:9443 \
+docker run -d -p 9080:9080 -p 9443:9443 \
   -v /${TRAVIS_BUILD_DIR}/.travis/wlp-server-template.xml:/config/server.xml \
   open-liberty:webProfile8
